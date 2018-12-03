ddc3c9925f4e213a8e2681e4fd417885652761ca
cass_dev <cass_dev@tc-apple.ru>
2018-12-03 09:44:53 +0500

    finished Chapter8

--- Chapter8.hpp
+++ Chapter8.hpp
@@ -144,6 +144,90 @@ process_widget(sw);                                 // process_widget видит
 }
 
 //------------------------------------------------------------------------------
+// 8.2 Рассмотрите применение размещения вместо вставки
+{
+
+
+template<class T,                           // Из стандарта
+         class Allocator = allocator<T>>    // С++11
+class vector {
+public:
+    // ...
+    void push_back(const T& x);             // Вставка lvalue
+    void push_back(T&& x);                  // Вставка rvalue
+    // ...
+};
+std::vector<std::string> vs;                // Контейнер std::string
+vs.push_back("xyzzy");                      // Добавление строкового литерала
+// Для вызова push_back со строковым литералом
+// компиляторы рассмотрят вызов следующим образом:
+vs.push_back(std::string("xyzzy");          // Создание временного
+// объекта std::string и передача его функции push_back
+
+
+// Чтобы избежать создание временного объекта:
+vs.emplace_back("xyzzy");   // Создает std::string в vs
+                            // непосредственно из "xyzzy"
+vs.emplace_back(50, 'x');   // Создает std::string из
+                            // 50 символов 'x'
+
+
+std::string queen_of_disco{"Donna Summer"};
+vs.push_back(queen_of_disco);       // Копирующее конструирование
+                                    // queen_of_disco в конце vs
+vs.emplace_back(queen_of_disco);    // Тоже самое
+
+
+std::vector<std::string> vs;        // Как и ранее
+// ...                              // Добавление элементов в vs
+vs.emplace(vs.begin(), "xyzzy");    // Добавление "xyzzy" в начало vs
+
+
+vs.emplace_back("xyzzy");   // Копирует новое значение в конец
+                            // контейнера; тип аргумента отличен от
+                            // типа, хранимого в контейнере;
+                            // контейнер не отвергает дубликаты
+vs.emplace_back(50, 'x');   // Тоже самое
+
+
+std::list<std::shared_ptr<Widget>> ptrs;
+void kill_widget(Widget* p_widget);
+
+// Использование push_back, с ресурсом все ОК
+ptrs.push_back(std::shared_ptr<Widget>(new Widget, kill_widget));
+ptrs.push_back({new Widget, kill_widget});
+// Использование emplace_back, потенциальная утечка ресурса
+ptrs.emplace_back(new Widget, kill_widget);
+// Более лучший стиль
+std::shared_ptr<Widget> spw(new Widget,     // Создание Widget и
+                           kill_widget);    // передача его spw
+ptrs.push_back(std::move(spw));             // Добавление spw
+                                            // как rvalue
+// Версия с emplace_back:
+ptrs.emplace_back(std::move(spw));
+
+
+std::vector<std::regex> regexes;
+regexes.emplace_back(nullptr);      // Компилируется, "все ок"
+std::regex r = nullptr;             // Ошибка! Не компилируется! Конструктор explicit
+regexes.push_back(nullptr);         // Ошибка! Не компилируется! Конструктор explicit
+std::regex upper_case_word("[A-Z]+");
+std::regex r(nullptr);              // Компилируется
+std::regex r1 = nullptr;            // Ошибка! Не компилируется!
+std::regex r2(nullptr);             // Компилируется
+
+
+// Вывод:
+regexes.emplace_back(nullptr);  // Компилируется. Прямая инициализация
+                                // разрешает использовать коннструктор
+                                // explicit std::regex, получающий указатель
+regexes.push_back(nullptr);     // Ошибка! Копирующая инициализация такие
+                                // конструкторы не использует
+
+
+}
+
+//------------------------------------------------------------------------------
 
 #endif // CHAPTER8_HPP
 
